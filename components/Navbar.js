2965987a9dd196eea5f3d5c3873550c0bc70e1e4
kumar8799parkash <kumar8799parkash@gmail.com>
2025-08-12 20:28:06 +0530

    updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
-      <div className="nav  h-[12vh]  border-2  border-black-500  w-screen flex items-center pl-10">
-        <div className="brand-cont">BRAND</div>
-        <div className="main-nav-cont flex gap-6">
-          <li>Home</li>
-          <li>Categories</li>
-          <li>Contact</li>
-          <li>About Us</li>
+      <div className="nav  h-[12vh]  w-screen flex items-center px-10 justify-between border-[1px] border-[#cbcbcb]">
+        <div className="brand-cont flex gap-2 items-center">
+          <div className="brand-image-cont"><img className='w-[40px]' src="/images/sonicEdgeLogo.png" alt="" /></div>
+          <div className="brand-name-cont">Sonic edge</div>
+        </div>
+        <div className="main-nav-cont flex gap-12 ">
+          <li><Link href="/">Home</Link></li>
+          <li className='flex '>Categories <img className='w-4' src="/images/dropDown.svg" alt="" /></li>
+          <li><Link href="/">Contact</Link></li>
+          <li><Link href="/">About Us</Link></li>
+        </div>
+
+        <div className="search-cont flex gap-2">
+          <div className="input-cont flex h-[37px] px-3 rounded-3xl bg-[#f4f5f7] text-[rgb(37,38,39)]">
+            <img className='w-5' src="/images/search.svg" alt="" />
+            <input type="text" className='ml-1 w-[15vw] text-[15px] outline-none' placeholder='Searh the product' />
+          </div>
+          
+          <img  src="/images/cart.svg" className='w-7 ml-2 cursor-pointer' alt="" />
         </div>
       </div>
     </div>
