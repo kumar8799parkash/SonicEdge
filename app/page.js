c5e44f8f0cd2e0c1c6731d3af922e13ca0796aad
kumar8799parkash <kumar8799parkash@gmail.com>
2025-08-17 06:56:59 +0530

    upadated

--- app/page.js
+++ app/page.js
@@ -1,13 +1,53 @@
+"use client"
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import './globals.css';
 
 export default function Home() {
+
+  const [mainImageSrc, setMainImageSrc] = useState("/images/mainImages/mainImage0.webp");
+  const [mainImageIndex, setMainImageIndex] = useState(0);
+  const [mainImagesArray, setMainImagesArray] = useState([]);
+
+  useEffect(() => {
+    async function fetchMainImages() {
+      const res = await fetch("/images/mainImages.json");
+      const data = await res.json();
+      setMainImagesArray(data);
+    }
+
+    fetchMainImages();
+  }, [])
+
+
+  useEffect(() => {
+    if (mainImagesArray.length === 0) return;
+
+    const interval = setInterval(() => {
+      setMainImageIndex((prev) => (prev + 1) % mainImagesArray.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [mainImagesArray]);
+
+
+  function changeMainImage() {
+    const len = mainImagesArray.length;
+    setMainImageIndex((mainImageIndex + 1) % len);
+    setMainImageSrc(`${mainImagesArray[mainImageIndex].source}`);
+  }
+
+  setInterval(() => {
+    changeMainImage();
+  }, 1000)
+
   return (
+
     <>
-      <Navbar/>
+      <Navbar />
       <div className="main-image-cont h-[88vh] w-screen bg-amber-400">
-        <img className="main-image h-[100%] w-[100%]" src="/images/mainImages/mainImage5.webp" alt="" />
+        <img className="main-image h-[100%] w-[100%]" src={mainImageSrc} alt="" />
       </div>
     </>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import './Navbar.css'
 
 const Navbar = () => {
+
+
   return (
     <div>
       <div className="nav  h-[12vh]  w-screen flex items-center px-10 justify-between border-[1px] border-[#cbcbcb]">
@@ -14,54 +16,54 @@ const Navbar = () => {
 
           <li className='main-nav-item' ><Link href="/">Home</Link></li>
 
-          <li className='main-nav-item drop-down' ><Link href="/">Categories</Link> <img className='w-4' src="/images/dropDown.svg" alt="" />
+          <li className='main-nav-item drop-down' ><Link href="/">Categories</Link> <img className='w-4' src="/images/SVGs/dropDown.svg" alt="" />
 
 
             <div className="content">
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/TruewirelessEarbuds.webp" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/TruewirelessEarbuds.webp" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">True Wireless Earbuds</div>
               </Link>
 
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/wirelessSpeakers.webp" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/wirelessSpeakers.webp" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Wireless Speakers</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/neckbands.png" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/neckbands.png" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Neckbands</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/smartWatches.webp" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/smartWatches.webp" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Smart Watches</div>
               </Link>
 
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/wiredHeadphones.png" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/wiredHeadphones.png" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Wired Headphones</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/wirelessHeadphones.webp" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/wirelessHeadphones.webp" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Wireless Headphones</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/wiredEarphones.webp" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/wiredEarphones.webp" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Wired Earphones</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/soundbars.png" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/soundbars.png" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Soundbars</div>
               </Link>
 
               <Link className='drop-down-item' href="/">
-                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/dropDownItemsImages/gamingHeadphones.png" alt="" /></div>
+                <div className="drop-down-item-image-cont h-[100%] w-[25%] flex items-center justify-center"><img className=' w-[65%]' src="/images/categoriesImages/gamingHeadphones.png" alt="" /></div>
                 <div className="drop-down-item-name-cont h-[100%] w-[75%] text-[13px] flex items-center">Gaming Headphones</div>
               </Link>
             </div>
@@ -76,11 +78,11 @@ const Navbar = () => {
 
         <div className="search-cont flex gap-2">
           <div className="input-cont flex h-[37px] px-3 rounded-3xl bg-[#f4f5f7] text-[rgb(37,38,39)]">
-            <img className='w-5' src="/images/search.svg" alt="" />
+            <img className='w-5' src="/images/SVGs/search.svg" alt="" />
             <input type="text" className='ml-1 w-[15vw] text-[15px] outline-none' placeholder='Searh the product' />
           </div>
 
-          <img src="/images/cart.svg" className='w-7 ml-2 cursor-pointer' alt="" />
+          <img src="/images/SVGs/cart.svg" className='w-7 ml-2 cursor-pointer' alt="" />
         </div>
       </div>
 
