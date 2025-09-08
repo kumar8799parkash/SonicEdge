9149b7e06bf9c6c9ede861f07939c85920853115
kumar8799parkash <kumar8799parkash@gmail.com>
2025-09-08 06:29:36 +0530

    updated

---
+++ components/SpecificCategoryPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SpecificCategoryPage = () => {
+  return (
+    <div>
+      <div className="category-heading-cont"></div>
+      <div className="filter-button-cont"></div>
+      <div className="items-cont">
+        <div className="specific-product"></div>
+      </div>
+    </div>
+  )
+}
+
+export default SpecificCategoryPage
---
+++ components/SpecificProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecificProduct = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpecificProduct
